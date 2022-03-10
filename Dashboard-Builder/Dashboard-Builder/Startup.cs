9b03e0048c3edcb4afe4e55e4a1b4ffdd8af15b5
Shaheen <mahmoud_shaheen@live.com>
2022-03-10 15:09:42 +0200

    use in memory DB

--- Dashboard-Builder/Dashboard-Builder/Startup.cs
+++ Dashboard-Builder/Dashboard-Builder/Startup.cs
@@ -26,12 +26,17 @@ namespace DashboardBuilder
             services.AddMvc().SetCompatibilityVersion(Latest);
             services.AddSwaggerGen(op=>op.SwaggerDoc("v1", new Info { Title = "Dashboard Builder API", Version = "v1.0" }));
 
+#if DEBUG
+            services.AddDbContext<DBContext>(options =>
+                    options.UseInMemoryDatabase());
+#else
             services.AddDbContext<DBContext>(options =>
                     options.UseLazyLoadingProxies().UseSqlServer(
                         Configuration.GetConnectionString("DBContext")));
+#endif
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, DBContext context)
         {
             app.UseStaticFiles();
 
@@ -55,6 +60,23 @@ namespace DashboardBuilder
 
             app.UseSwagger();
             app.UseSwaggerUI(c=>c.SwaggerEndpoint("/swagger/DashboardBuilder/swagger.json", "Dashboard Builder"));
+            SeedData(context);
+        }
+
+        public void SeedData(DBContext context)
+        {
+            if (context.Dashboards.Any())
+            {
+                // Already has data
+                return;
+            }
+            context.Dashboards.Add(new Models.Dashboard
+            {
+                Name = "Main Dashboard",
+                Canvas = "[{\"Title\":\"Line Chart\",\"Rows\":10,\"Cols\":17,\"X\":0,\"Y\":0,\"Link\":\"http://localhost:8080/api/Charts/timeseries\",\"Type\":\"line\"},{\"Title\":\"Bar Chart\",\"Rows\":10,\"Cols\":17,\"X\":17,\"Y\":0,\"Link\":\"http://localhost:8080/api/Charts/timeseries\",\"Type\":\"bar\"},{\"Title\":\"Pie Chart\",\"Rows\":10,\"Cols\":17,\"X\":0,\"Y\":10,\"Link\":\"http://localhost:8080/api/Charts/data\",\"Type\":\"pie\"},{\"Title\":\"Radar Chart\",\"Rows\":10,\"Cols\":17,\"X\":17,\"Y\":10,\"Link\":\"http://localhost:8080/api/Charts/data\",\"Type\":\"radar\"},{\"Title\":\"Doughnut Chart\",\"Rows\":10,\"Cols\":17,\"X\":0,\"Y\":20,\"Link\":\"http://localhost:8080/api/Charts/data\",\"Type\":\"doughnut\"},{\"Title\":\"Polar Chart\",\"Rows\":10,\"Cols\":17,\"X\":17,\"Y\":20,\"Link\":\"http://localhost:8080/api/Charts/data\",\"Type\":\"polarArea\"}]",
+            });
+            context.SaveChanges();
+
         }
 
     }
